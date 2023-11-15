7c185a4a43860724cfc3158d0cdc832d9fae0d0f
AlbertoSchiaffino00 <alberto.schiaffino00@gmail.com>
2023-11-15 19:00:28 +0100

    Working tiling for square matrix training

--- sw/training/gradient/src/gradient.h
+++ sw/training/gradient/src/gradient.h
@@ -18,8 +18,6 @@ void backpropagation_one_core(DATA_TYPE *I, DATA_TYPE *W, DATA_TYPE *B, DATA_TYP
              double e,int m, int n, int k);
 void backpropagation_baseline_multicore(DATA_TYPE *I, DATA_TYPE *W, DATA_TYPE *B, DATA_TYPE *E,
              double e,int m, int n, int k);
-void backpropagation_multicore(DATA_TYPE *I, DATA_TYPE *W, DATA_TYPE *B, DATA_TYPE *E,
-             double e,int m, int n, int k);
 
 // I[m][k] inputs
 // W[k][n] weights
@@ -91,14 +89,17 @@ void backpropagation_baseline_multicore(DATA_TYPE *local_i, DATA_TYPE *local_w, 
     
 }
 
+//TODO: doesnt work with small sizes. the first loop of the biases. I added /2 for tiling
 void backpropagation_multicore(DATA_TYPE *local_i, DATA_TYPE *local_w, DATA_TYPE *local_b, DATA_TYPE *local_e,
-             double e,int m, int n, int k){
+             double e,int m, int n, int k, int k_tiling, int n_tiling){
 
-    uint32_t c, lb, ub,i,j,z;
+    uint32_t c, lb, ub,i,j,z, dim;
     DATA_TYPE sum;
     const uint32_t compute_num = snrt_cluster_compute_core_num();
     const uint32_t compute_id = snrt_cluster_core_idx();
-    
+ 
+
+    //next instructions useless but bugs dont let me take them out on odd iterations
     c = CEIL(n*m, compute_num);
     lb = c * compute_id;
     ub = MIN((c * (compute_id + 1)), n*m);
@@ -106,28 +107,34 @@ void backpropagation_multicore(DATA_TYPE *local_i, DATA_TYPE *local_w, DATA_TYPE
     lbs[compute_id]=lb;
     ubs[compute_id]=ub;
 
-    uint32_t dim = ub-lb;  
-    //dC/dB = E 
-    //B[m][n] = old(B[m][n])- e*dC/dB[m][n]
-    if(dim>0){
-        snrt_ssr_loop_1d(SNRT_SSR_DM0, dim, sizeof(DATA_TYPE));
-        snrt_ssr_loop_1d(SNRT_SSR_DM1,dim, sizeof(DATA_TYPE));
+    dim = ub-lb;  
 
-        snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_1D, local_b+lb);
-        snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_1D, local_e+lb);
-        snrt_ssr_write(SNRT_SSR_DM0, SNRT_SSR_1D, local_b+lb);
+    if(k_tiling==n_tiling){      
+        //dC/dB = E 
+        //B[m][n] = old(B[m][n])- e*dC/dB[m][n]
+        if(dim>0){
+            snrt_ssr_loop_1d(SNRT_SSR_DM0, dim, sizeof(DATA_TYPE));
+            snrt_ssr_loop_1d(SNRT_SSR_DM1,dim, sizeof(DATA_TYPE));
+            snrt_ssr_loop_1d(SNRT_SSR_DM2, dim, sizeof(DATA_TYPE));
 
-        snrt_ssr_enable();
-        asm volatile(
-            "frep.o %[n_frep], 1, 0, 0 \n"
-            "fmadd.d ft0, %[neg_e], ft1, ft0\n"
-            :
-            : [ n_frep ] "r"(dim-1), [ neg_e ] "f"(-e) //define variables used 
-            : "ft0", "ft1", "ft2", "memory"); //registered touched
 
-        snrt_fpu_fence();
-        snrt_ssr_disable();
+            snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_1D, local_b+lb);
+            snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_1D, local_e+lb);
+            snrt_ssr_write(SNRT_SSR_DM2, SNRT_SSR_1D, local_b+lb);
+
+            snrt_ssr_enable();
+            asm volatile(
+                "frep.o %[n_frep], 1, 0, 0 \n"
+                "fmadd.d ft2, %[neg_e], ft1, ft0\n"
+                :
+                : [ n_frep ] "r"(dim-1), [ neg_e ] "f"(-e) //define variables used 
+                : "ft0", "ft1", "ft2", "memory"); //registered touched
+
+            snrt_fpu_fence();
+            snrt_ssr_disable();
+        }
     }
+    snrt_cluster_hw_barrier();
 
     c = CEIL(k, compute_num);
     lb = c * compute_id;
@@ -152,21 +159,20 @@ void backpropagation_multicore(DATA_TYPE *local_i, DATA_TYPE *local_w, DATA_TYPE
             :
             : [ n_frep ] "r"(n-1), [ neg_e ] "f"(-e), [I] "f"(local_i[i])
             :"ft0", "ft1","ft2", "memory");
+        
+        snrt_fpu_fence();
+        snrt_ssr_disable();
     }
 
 
         //I liked this one more
         // snrt_ssr_loop_2d(SNRT_SSR_DM1,n,k,0,sizeof(DATA_TYPE));//I_t
         // snrt_ssr_loop_2d(SNRT_SSR_DM2,n,k,sizeof(DATA_TYPE),0);//E
-        // snrt_ssr_loop_2d(SNRT_SSR_DM0,2,n*k,0,sizeof(DATA_TYPE));//W
-        
-
+        // snrt_ssr_loop_2d(SNRT_SSR_DM0,2,n*k,0,sizeof(DATA_TYPE));//W       
         // snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_2D, local_w); 
         // snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_2D, local_i);            
         // snrt_ssr_read(SNRT_SSR_DM2, SNRT_SSR_2D, local_e); 
-        // snrt_ssr_write(SNRT_SSR_DM0, SNRT_SSR_2D, local_w);   
-
-        
+        // snrt_ssr_write(SNRT_SSR_DM0, SNRT_SSR_2D, local_w);      
         // snrt_ssr_enable();
         // asm volatile(
         //     "frep.o %[n_frep], 2, 0, 0 \n" //rep_loop, length_loop, stagger_count, dtagger [rd|rs1|rs2|rs3]
@@ -175,7 +181,6 @@ void backpropagation_multicore(DATA_TYPE *local_i, DATA_TYPE *local_w, DATA_TYPE
         //     :  
         //     : [ n_frep ] "r"(k*n-1), [ neg_e ] "f"(-e), [ addr ] "r"(local_w)//define variables used 
         //     : "ft0", "ft1","fa0","fa1", "memory"); //registered touched
-
         // snrt_fpu_fence();
         // snrt_ssr_disable();
 }
--- sw/training/gradient/src/main.c
+++ sw/training/gradient/src/main.c
@@ -2,11 +2,16 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <math.h>
 #include "snrt.h"
 #include "gradient.h"
 #include "data.h"
 
 //#define SINGLE_CORE
+
+//TODO: modify such that dim of tiling is not fixed. 
+//corner cases. At the moment it works only with square matrixes (k==n)
+//it is still missing the check on dimension to decide oh much to bring on TCDM
 
 int main(int argc, char *argv[]) {
     #ifdef SINGLE_CORE
@@ -17,10 +22,17 @@ int main(int argc, char *argv[]) {
     }
     #else
     // Allocate space in TCDM
-    uint32_t size_i = M * K * sizeof(DATA_TYPE);
-    uint32_t size_w = K * N * sizeof(DATA_TYPE);
-    uint32_t size_b = M * N * sizeof(DATA_TYPE);
-    uint32_t size_e = M * N * sizeof(DATA_TYPE);
+    // uint32_t size_i = M * K * sizeof(DATA_TYPE);
+    // uint32_t size_w = K * N * sizeof(DATA_TYPE);
+    // uint32_t size_b = M * N * sizeof(DATA_TYPE);
+    // uint32_t size_e = M * N * sizeof(DATA_TYPE);
+
+    uint32_t fix_tiling_size = K/2;//TODO: udjust 
+
+    uint32_t size_i = fix_tiling_size * sizeof(DATA_TYPE);//shoud use sqrt, but it is 8*8 at the moment
+    uint32_t size_w = fix_tiling_size*fix_tiling_size * sizeof(DATA_TYPE);
+    uint32_t size_b = fix_tiling_size * sizeof(DATA_TYPE);
+    uint32_t size_e = fix_tiling_size * sizeof(DATA_TYPE);
 
 
     void *local_I, *local_W, *local_B, *local_E;
@@ -36,30 +48,57 @@ int main(int argc, char *argv[]) {
     remote_W = W;
     remote_E = E;
 
-    // Copy data in TCDM
-    if (snrt_is_dm_core()) {
-        snrt_dma_start_1d(local_I, remote_I, size_i);
-        snrt_dma_start_1d(local_W, remote_W, size_w);
-        snrt_dma_start_1d(local_B, remote_B, size_b);
-        snrt_dma_start_1d(local_E, remote_E, size_e);
-        snrt_dma_wait_all();
-    }
-    snrt_cluster_hw_barrier();
+    for(int i=0;i<K/fix_tiling_size;i++){
+        for(int j=0;j<N/fix_tiling_size;j++){
+            // Copy data in TCDM
+            if (snrt_is_dm_core()) {
+                snrt_dma_start_1d(local_I, remote_I+i*size_i, size_i);
+                for(int z=0;z<fix_tiling_size;z++){
+                    snrt_dma_start_1d(local_W + size_w/fix_tiling_size*z,
+                     remote_W + (z*N)*sizeof(DATA_TYPE)+j*size_w/fix_tiling_size + i*fix_tiling_size*N*sizeof(DATA_TYPE),
+                     size_w/fix_tiling_size);
+                }
+                snrt_dma_start_1d(local_B, remote_B+j*size_b, size_b);
+                snrt_dma_start_1d(local_E, remote_E+j*size_e, size_e);
+                snrt_dma_wait_all();
+            }
+            snrt_cluster_hw_barrier();
 
 
-    if(!snrt_is_dm_core()){
-        backpropagation_baseline_multicore((double*)local_I,(double*)local_W,(double*)local_B,(double*)local_E,e,M,N,K);
-        //backpropagation_multicore((double*)local_I,(double*)local_W,(double*)local_B,(double*)local_E,e,M,N,K);
-        snrt_fpu_fence();
-    }
+            if(!snrt_is_dm_core()){
+        //            snrt_mcycle();
+                backpropagation_multicore((double*)local_I,(double*)local_W,(double*)local_B,(double*)local_E,e,M,fix_tiling_size,fix_tiling_size,i,j);
+                snrt_fpu_fence();
+        //            snrt_mcycle();
+            }else{
+                snrt_cluster_hw_barrier();//there is a barrier inside the function of the cluster. DMA core needs to adjust to that too
+            }
 
-    snrt_cluster_hw_barrier();
+            snrt_cluster_hw_barrier();
 
 
-    if (snrt_is_dm_core()) {
-        snrt_dma_start_1d(remote_W, local_W, size_w);
-        snrt_dma_start_1d(remote_B, local_B, size_b);
-        snrt_dma_wait_all();
+            if (snrt_is_dm_core()) {
+                for(int z=0;z<fix_tiling_size;z++){
+                    snrt_dma_start_1d(remote_W + (z*N)*sizeof(DATA_TYPE)+j*size_w/fix_tiling_size+ i*fix_tiling_size*N*sizeof(DATA_TYPE),
+                     local_W + size_w/fix_tiling_size*z,
+                     size_w/fix_tiling_size);
+                }
+                if(i==j)
+                    snrt_dma_start_1d(remote_B+j*size_b, local_B, size_b);
+                snrt_dma_wait_all();
+            }
+            snrt_cluster_hw_barrier();
+        }
     }
     #endif
 }   
+
+
+
+        // while(1){
+        //     asm volatile(
+        //     "add a0, a0, %[zero]\n"
+        //     :
+        //     :[ zero ] "r"(0)
+        //     : "a0");
+        // }
